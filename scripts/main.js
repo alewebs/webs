ecabe4e6e53ab4edb30d60e5fad2f93bb5b80020
alewebs <75922695+alewebs@users.noreply.github.com>
2022-03-17 01:23:07 -0300

    cambio anclas

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,6 @@
 // Add your javascript here
+
+//Borrar hash de la url de los anclas
+window.onhashchange = function () {
+    window.history.pushState('', document.title, window.location.pathname)
+  }
