0ef52233ced5849d59e81a68e7c1fd68fe2fbc32
alewebs <75922695+alewebs@users.noreply.github.com>
2022-03-17 01:34:02 -0300

    Delete main.js

--- scripts/main.js
+++
@@ -1,6 +0,0 @@
-// Add your javascript here
-
-//Borrar hash de la url de los anclas
-window.onhashchange = function () {
-    window.history.pushState('', document.title, window.location.pathname)
-  }
